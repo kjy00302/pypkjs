355b54856212989e4d9b43fbf3ccf1640659ae9a
kjy00302 <kjy00302@gmail.com>
2021-02-28 04:26:50 +0900

    Update required packages to latest version

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@ __author__ = 'katharine'
 from setuptools import setup, find_packages
 
 requirements = [
-    'backports.ssl-match-hostname==3.4.0.2',
     'gevent>=1.5.0',
-    'gevent-websocket==0.9.3',
+    'gevent-websocket>=0.9.3',
     'greenlet>=0.4.7',
-    'peewee==2.4.7',
-    'pygeoip==0.3.2',
-    'pypng==0.0.17',
-    'python-dateutil==2.4.1',
-    'requests==2.7.0',
-    'sh==1.09',
-    'six==1.9.0',
-    'websocket-client==0.32.0',
+    'peewee>=3.0.0',
+    'pygeoip>=0.3.2',
+    'pypng>=0.0.17',
+    'python-dateutil>=2.4.1',
+    'requests>=2.7.0',
+    'sh>=1.09',
+    'six>=1.9.0',
+    'websocket-client>=0.32.0',
     'libpebble2>=0.0.20',
-    'netaddr==0.7.18'
+    'netaddr>=0.7.18'
 ]
 
 __version__= None  # Overwritten by executing version.py.
