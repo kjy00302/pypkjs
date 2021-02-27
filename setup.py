9b073aab2c36d8e850845ab6cdcf1e5bc2892264
kjy00302 <kjy00302@gmail.com>
2021-02-28 03:18:59 +0900

    Replace bundled PyV8 to STPyV8

--- setup.py
+++ setup.py
@@ -21,10 +21,6 @@ setup(name='pypkjs',
       install_requires=requirements,
       package_data={
           'pypkjs.javascript.navigator': ['GeoLiteCity.dat'],
-          'pypkjs.PyV8.darwin64': ['_PyV8.so'],
-          'pypkjs.PyV8.linux32': ['_PyV8.so'],
-          'pypkjs.PyV8.linux64': ['_PyV8.so', 'libboost_python.so.1.53.0', 'libboost_system-mt.so.1.53.0',
-                           'libboost_thread-mt.so.1.53.0'],
           'pypkjs.timeline': ['layouts.json'],
       },
       entry_points={
